5fd12107848043237076830858c17a7f671bcf99
console1928 <riverflow1928@yandex.ru>
2025-04-06 10:16:02 +0300

    Add eslintrc, prettierrc, vscode settings and react swiper

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
                 ],
             },
             {
+                test: /\.(sass|css)$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
