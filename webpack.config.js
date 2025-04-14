54a36e76fc2180476fa2e74b6dd9f4b2fdb18407
console1928 <riverflow1928@yandex.ru>
2025-04-14 14:00:27 +0300

    Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,60 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const loader = require('sass-loader')
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.module\.scss$/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                namedExport: false,
-                                localIdentName: '[name]__[local]__[hash:base64:5]'
-                            }
-                        }
-                    },
-                    { loader: 'sass-loader' }
-                ],
-            },
-            {
-                test: /\.(sass|css)$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.module\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                namedExport: false,
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
             }
+          },
+          { loader: 'sass-loader' }
         ]
+      },
+      {
+        test: /\.(sass|css)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.(ttf)$/,
+        type: 'asset/inline'
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
     },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        hot: true,
-        port: 3000,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-    ],
-};
+    hot: true,
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
